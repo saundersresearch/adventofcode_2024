ba3205707f1dc6a4beef897e1a477a3fb7f32ba0
saundersresearch <adam.m.saunders@vanderbilt.edu>
2024-12-10 21:19:22 -0600

    Day 10

---
+++ code/day10.py
@@ -0,0 +1,91 @@
+test_string = """89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+"""
+
+input_string = test_string
+with open("inputs/day10.txt") as f:
+    input_string = f.read()
+
+topographic_map = [list(line) for line in input_string.splitlines()]
+print("\n".join(["".join(row) for row in topographic_map]))
+
+### Part 1
+# Find zeros
+zeros = [
+    (row_idx, col_idx)
+    for row_idx, row in enumerate(topographic_map)
+    for col_idx, col in enumerate(row)
+    if col == "0"
+]
+# print(zeros)
+
+
+def get_neighbors(array, idx, n):
+    """Look for cardinal direction neighbors of value n at idx"""
+    neighbors = [
+        (idx[0] + 1, idx[1]),
+        (idx[0] - 1, idx[1]),
+        (idx[0], idx[1] + 1),
+        (idx[0], idx[1] - 1),
+    ]
+    neighbors = [
+        neighbor
+        for neighbor in neighbors
+        if neighbor[0] >= 0
+        and neighbor[0] < len(array)
+        and neighbor[1] >= 0
+        and neighbor[1] < len(array[0])
+        and array[neighbor[0]][neighbor[1]] == n
+    ]
+    return neighbors
+
+
+def find_paths(array, idx, final_val):
+    """Find paths from value at idx to final_val"""
+    value = array[idx[0]][idx[1]]
+    neighbors = set([idx])
+    while int(value) < final_val:
+        update = []
+        for neighbor_idx in neighbors:
+            update.extend(get_neighbors(array, neighbor_idx, str(int(value) + 1)))
+        neighbors = set(update)
+        value = str(int(value) + 1)
+
+    return neighbors
+
+
+trail_scores = [len(find_paths(topographic_map, zero, 9)) for zero in zeros]
+print(f"Sum of trail scores: {sum(trail_scores)}")
+
+### Part 2
+# Find zeros
+zeros = [
+    (row_idx, col_idx)
+    for row_idx, row in enumerate(topographic_map)
+    for col_idx, col in enumerate(row)
+    if col == "0"
+]
+
+
+def find_n_paths(array, idx, final_val):
+    """Find paths from value at idx to final_val"""
+    value = array[idx[0]][idx[1]]
+    neighbors = [idx]
+    while int(value) < final_val:
+        update = []
+        for neighbor_idx in neighbors:
+            update.extend(get_neighbors(array, neighbor_idx, str(int(value) + 1)))
+        neighbors = update.copy()
+        value = str(int(value) + 1)
+
+    return len(neighbors)
+
+
+trail_ratings = [find_n_paths(topographic_map, zero, 9) for zero in zeros]
+print(f"Sum of trailhead ratings: {sum(trail_ratings)}")
--- code/day9.py
+++ code/day9.py
@@ -1,8 +1,7 @@
 import heapq
-from itertools import groupby
 from collections import defaultdict
+from itertools import groupby
 
-import line_profiler
 import numpy as np
 from tqdm import tqdm
 
@@ -59,10 +58,6 @@ for i, char in enumerate(input_string):
         blank = "."
         disk_blocks.extend([-1] * int(char))
 
-# disk_blocks = np.array(disk_blocks)
-print(disk_blocks)
-
-
 # Create a heap of all the gaps, sorted by gap length
 def create_blank_heaps(disk_blocks):
     blank_heaps = [[] for i in range(10)]
@@ -77,10 +72,7 @@ def create_blank_heaps(disk_blocks):
     [heapq.heapify(blank_heap) for blank_heap in blank_heaps]
     return blank_heaps
 
-
 blank_heaps = create_blank_heaps(disk_blocks)
-print(blank_heaps, "".join([str(d) for d in disk_blocks]).replace("-1", "."))
-print(disk_blocks)
 
 # Create locations of each file id and store in dict file_id: (index, length)
 file_loc_dict = defaultdict(lambda: {"start": None, "size": 0})
@@ -88,46 +80,42 @@ for idx, file_id in enumerate(disk_blocks):
     if file_id != -1:
         if file_loc_dict[file_id]["start"] is None:
             file_loc_dict[file_id]["start"] = idx  # Set start index
-        file_loc_dict[file_id]["size"] += 1     # Increment size
+        file_loc_dict[file_id]["size"] += 1  # Increment size
 
-# @line_profiler.profile
-def get_new_seq():
-    # Compress sequence by iterating down files and moving whole file IDs that fit into blank spaces
-    for file_id in tqdm(range(max(disk_blocks), -1, -1)):
-        # Where is file?
-        file_start = file_loc_dict[file_id]["start"]
-        file_size = file_loc_dict[file_id]["size"]
+# Compress sequence by iterating down files and moving whole file IDs that fit into blank spaces
+for file_id in range(max(disk_blocks), -1, -1):
+    # Where is file?
+    file_start = file_loc_dict[file_id]["start"]
+    file_size = file_loc_dict[file_id]["size"]
 
-        # Is there a spot that fits this?
-        # Get a list of smallest index for each
-        leftmost_indices = [
-            heap[0] if len(heap) > 0 and i >= file_size else len(disk_blocks)
-            for i, heap in enumerate(blank_heaps)
-        ]
+    # Is there a spot that fits this?
+    # Get a list of smallest index for each
+    leftmost_indices = [
+        heap[0] if len(heap) > 0 and i >= file_size else len(disk_blocks)
+        for i, heap in enumerate(blank_heaps)
+    ]
 
-        # Find min that fits
-        min_index = min(leftmost_indices)
-        min_heap_index = leftmost_indices.index(min_index)
+    # Find min that fits
+    min_index = min(leftmost_indices)
+    min_heap_index = leftmost_indices.index(min_index)
 
-        if min_index < len(disk_blocks) and min_index < file_start:
-            new_idx = heapq.heappop(blank_heaps[min_heap_index])
+    if min_index < len(disk_blocks) and min_index < file_start:
+        new_idx = heapq.heappop(blank_heaps[min_heap_index])
 
-            # Put into space
-            disk_blocks[new_idx : new_idx + file_size] = [file_id] * file_size
+        # Put into space
+        disk_blocks[new_idx : new_idx + file_size] = [file_id] * file_size
 
-            # Remove from old space
-            disk_blocks[file_start : file_start + file_size] = [-1] * file_size
+        # Remove from old space
+        disk_blocks[file_start : file_start + file_size] = [-1] * file_size
 
-            # Update heap
-            new_size = min_heap_index - file_size
-            if new_size > 0:
-                heapq.heappush(blank_heaps[new_size], new_idx + file_size)
+        # Update heap
+        new_size = min_heap_index - file_size
+        if new_size > 0:
+            heapq.heappush(blank_heaps[new_size], new_idx + file_size)
 
-            # Update dict
-            file_loc_dict[file_id]["start"] = new_idx
-            file_loc_dict[file_id]["size"] = file_size
-
-        # print(blank_heaps, "".join([str(d) for d in disk_blocks]).replace("-1", "."))
+        # Update dict
+        file_loc_dict[file_id]["start"] = new_idx
+        file_loc_dict[file_id]["size"] = file_size
 
 # Checksum is sum of id * location
 checksum = 0
